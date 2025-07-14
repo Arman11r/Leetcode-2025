c096de65947802308dc461f6b39e84fb57c542fa
Arman Ahmed <137074653+Arman11r@users.noreply.github.com>
2025-07-14 14:55:37 +0530

    Time: 0 ms (100%), Space: 41.5 MB (15.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,19 +2,31 @@ class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
-        return ans(nums,0,dp);
+        //return ans(nums,nums.length-1,dp);
+        if(nums.length == 1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int rob = nums[i]+dp[i-2];
+            int dontrob = dp[i-1];
+            dp[i] = Math.max(rob,dontrob);
+        }
+        return dp[dp.length-1];
         
     }
 
-    public int ans(int[] nums, int i,int[] dp){
-        if(i>=nums.length){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int robbinggggg = nums[i] + ans(nums,i+2,dp);
-        int notrobbing = ans(nums,i+1,dp);
-        return dp[i]=Math.max(robbinggggg,notrobbing);
-    }
+
+    // public int ans(int[] nums, int i,int[] dp){
+    //     if(i>=nums.length){
+    //         return 0;
+    //     }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+    //     int robbinggggg = nums[i] + ans(nums,i+2,dp);
+    //     int notrobbing = ans(nums,i+1,dp);
+    //     return dp[i]=Math.max(robbinggggg,notrobbing);
+    // }
 }
